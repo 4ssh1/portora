ca205d9cfe6600247b2e782d9760e3066a39bd47
Nzeshi Sarah <sarahnzeshi05@gmail.com>
2025-07-06 20:30:58 +0100

    made the debugger function more clean

--- utils/debounce.ts
+++ utils/debounce.ts
@@ -1,13 +1,15 @@
-export function debounce<T extends (...args: any[]) => void>(
-  func: T,
+export function debounce<F extends (...args: Parameters<F>) => ReturnType<F>>(
+  fn: F,
   delay: number
-): (...args: Parameters<T>) => void {
-  let timeout: ReturnType<typeof setTimeout>
-  return (...args: Parameters<T>) => {
-    clearTimeout(timeout)
-    timeout = setTimeout(() => func(...args), delay)
+): (...args: Parameters<F>) => void {
+  let timeoutId: ReturnType<typeof setTimeout>
+
+  return (...args: Parameters<F>) => {
+    clearTimeout(timeoutId)
+    timeoutId = setTimeout(() => fn(...args), delay)
   }
 }
+
 
 // Debouncing ensures that a function is not called too often — it waits for a pause in rapid events (like typing or scrolling) before running the function.
 // 	Wait until user stops doing something
